e0d62458ccca9250539d280df798893d678fb96d
Denis Godhani <godhanidenis@gmail.com>
2025-06-25 10:13:15 +0530

    in footer add home screen redirection

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -4,8 +4,10 @@ import TaxIntelliLogo from "../../assets/TaxIntelliLogo.png";
 import XIcon from "../../assets/socialMedia/x.png";
 import InstagramIcon from "../../assets/socialMedia/instagram.png";
 import YoutubeIcon from "../../assets/socialMedia/youtube.png";
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+  const navigate = useNavigate();
   const menuItems = {
     "Privacy Policy": "Privacy Policy",
     "Terms of Service": "Terms of Service",
@@ -32,6 +34,7 @@ const Footer = () => {
           className="h-[37px] w-[145px] hidden sm:flex cursor-pointer"
           draggable="false"
           alt="logo picture"
+          onClick={() => navigate("/")}
         />
 
         <Box className="lg:gap-6 gap-3 rounded-lg flex flex-col sm:flex-row items-start sm:items-center justify-center !flex-wrap">
@@ -60,6 +63,7 @@ const Footer = () => {
                 alt={icon.alt}
                 className="cursor-pointer hover:scale-105 duration-300 transition-all !h-6 !w-6"
                 draggable="false"
+                onClick={() => navigate("/")}
               />
             </Box>
           ))}
